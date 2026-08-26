3f1a6139e26b22e7e98e439225450ab02240cf78
shanika Jayawardane <shanikauwu@gmail.com>
2026-08-25 22:05:05 -0600

    add the storymap section

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
-import Hero from "./components/Hero";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Experience from "./components/Experience";
-import Education from "./components/Education";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import ProjectDetails from "./pages/ProjectDetails";
-
-function Home() {
-  return (
-    <>
-      <Hero />
-      <Skills />
-      <Projects />
-    </>
-  );
-}
-
-function About() {
-  return (
-    <>
-      <Experience />
-      <Education />
-    </>
-  );
-}
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen flex flex-col">
+      <div className="flex min-h-screen flex-col">
+        {/* Navbar */}
         <Navbar />
 
+        {/* Page Content */}
         <main className="mt-24 flex-1">
           <Routes>
+            {/* Home Page */}
             <Route path="/" element={<Home />} />
+
+            {/* About Page */}
             <Route path="/about" element={<About />} />
+
+            {/* Project Details Page */}
             <Route path="/projects/:projectId" element={<ProjectDetails />} />
           </Routes>
         </main>
 
+        {/* Footer */}
         <Footer />
       </div>
     </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from "../components/Hero";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
+import StoryMaps from "../components/StoryMaps";
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <Hero />
       <Skills />
       <Projects />
+      <StoryMaps />
     </main>
   );
 }
